8139e194c512ba94ee3370288e96c91bd1fffb07
animesh3640 <karne.ani@gmail.com>
2023-06-19 16:25:34 +0530

    t

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import './../styles/App.css';
-import Child from "./Child";
+import Parent from "./Parent"
 const App = () => {
   return (
-        <div className="parent">
-          <h1>Parent Component</h1>
-            <Child></Child>
+        <div>
+          <Parent></Parent>
         </div>
   )
 }
---
+++ src/components/Parent.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Child from "./Child"
+const Parent =()=>{
+    return(
+        <div
+        className="parent">
+            <h1>Parent Component</h1>
+            <Child></Child>
+        </div>
+    )
+}
