74033768bc26e489f56c3e0177dcb577eae167ba
animesh3640 <karne.ani@gmail.com>
2023-06-19 16:42:56 +0530

    final 2

--- src/components/Child.js
+++ src/components/Child.js
@@ -10,7 +10,7 @@ const Child = () =>{
                 (showModal)&&(
                     <div>
                         <h3>Modal Content</h3>
-                        <p>This is modal content</p>
+                        <p>This is the modal content.</p>
                     </div>
                 )
             }
